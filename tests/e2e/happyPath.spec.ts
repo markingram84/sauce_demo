f3afbaaa75d401d546a76fa18267af7d555c0d8d
Mark Ingram <markingram84@outlook.com>
2026-01-27 13:20:07 +0000

    start of refactoring

--- tests/e2e/happyPath.spec.ts
+++ tests/e2e/happyPath.spec.ts
@@ -4,27 +4,23 @@ const lastname = process.env.LASTNAME || '';
 const zipcode = process.env.ZIPCODE || '';
 
 test(`Happy Path Test`, async ({ authenticatedPage, ProductsPage, YourCartPage, CustomerDetailsPage, CheckoutOverviewPage, OverviewPage }) => {
-  await expect(authenticatedPage.page).toHaveTitle("Swag Labs");
   await ProductsPage.open();
-  await ProductsPage.Products.Tiles.AddToCartButtons.nth(0).click();
-  await ProductsPage.Products.Tiles.AddToCartButtons.nth(2).click();
-  await ProductsPage.Products.Tiles.AddToCartButtons.nth(4).click();
-  await ProductsPage.Products.Tiles.AddToCartButtons.nth(3).click();
-  await ProductsPage.Products.Tiles.RemoveFromCartButtons.nth(2).click();
+  const allButtons = ProductsPage.Products.Tiles.AddToCartButtons;
+  const count = await allButtons.count();
+  for (let i = 0; i < count - 1; i++) {
+    await allButtons.nth(i).click();
+  }
+  // await ProductsPage.Products.Tiles.RemoveFromCartButtons.nth(2).click();
   await ProductsPage.Cart.Component.Icon.click();
-  await expect(authenticatedPage.page).toHaveURL(/.*cart.html/);
-  await expect(YourCartPage.Basket.Component.Item).toHaveCount(3);
+  await expect(YourCartPage.Basket.Component.Item).toHaveCount(5); //Is this what I want?
   await YourCartPage.Basket.Component.CheckoutButton.click();
-  await expect(authenticatedPage.page).toHaveURL(/.*checkout-step-one.html/);
-  await CustomerDetailsPage.CustomerDetails.Fields.FirstName.fill(firstname);
+  await CustomerDetailsPage.CustomerDetails.Fields.FirstName.fill(firstname); //Can I do this in one line? 
   await CustomerDetailsPage.CustomerDetails.Fields.LastName.fill(lastname);
   await CustomerDetailsPage.CustomerDetails.Fields.ZipCode.fill(zipcode);
   await CustomerDetailsPage.CustomerDetails.Buttons.Continue.click();
-  await expect(authenticatedPage.page).toHaveURL(/.*checkout-step-two.html/);
   await expect(CheckoutOverviewPage.CheckoutOverview.Fields.Total).toBeVisible();
-  await expect(CheckoutOverviewPage.CheckoutOverview.Fields.Total).toHaveText(/Total:\s*\$\s*58\.29/);
+  // await expect(CheckoutOverviewPage.CheckoutOverview.Fields.Total).toHaveText(/Total:\s*\$\s*58\.29/); //Can I mock this?
   await CheckoutOverviewPage.CheckoutOverview.Buttons.Finish.click();
-  await expect(authenticatedPage.page).toHaveURL(/.*checkout-complete.html/);
   await expect(OverviewPage.Overview.Fields.Complete).toBeVisible();
   await OverviewPage.Overview.Buttons.BackHome.click();
   await expect(authenticatedPage.page).toHaveURL(/.*inventory.html/);
